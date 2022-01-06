8b46e00def860790a8ce41682c576c0baa89b8c5
Bruno Marcelino <brunomarcelino@ufmg.br>
2022-01-06 00:45:56 -0300

    new exercises folder

--- exercícios/resumos class.py
+++ exercícios/resumos class.py
@@ -1,3 +1,4 @@
+# CLASSES
 class Carro:
     #modelo empresa ano
     def __str__(self): # O que será mostrado quando printarmos o objeto
@@ -19,3 +20,32 @@ print(carro.ano) # retorna um atributo do carro
 
 carro.mudar_ano(2001) # retorna um método aplicável ao carro 
 print(carro.ano) # a função alterou o atributo do carro 
+
+# DIFERENÇA ENTRE FUNÇÕES COM E SEM CLASSES ASSOCIADAS
+#funções sem classes
+def somar(a,b):
+    return(a+b)
+
+def multiplicar(a,b):
+    return(a*b)
+
+#com classes
+class Calculadora:
+    
+    def somarc(self,a,b):
+        return(a+b)
+
+    def multiplicarc(self,a,b):
+        return(a*b)
+        
+    a = 6
+    b = 8 #só existem dentro da classe
+
+calculadora = Calculadora()
+
+print(calculadora.somarc(5,7))
+
+print(calculadora.a)
+
+calculadora.a = 20
+print(calculadora.multiplicarc(calculadora.a,calculadora.b))
